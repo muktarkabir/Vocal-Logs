d43885c6235c3e2b91e27d7e713321f6cf3bdb68
muktarkabirgdsc <mukhtarkabir102@gmail.com>
2023-12-25 23:29:37 +0100

    Get the transcription to work

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-//Setting initial title of the
+//Getting titleSpace
 const titleSpace = document.querySelector(".title");
+//Setting titleSpace
 if (titleSpace.textContent == "") {
 	titleSpace.textContent = new Date().toUTCString();
 }
 //Getting the textArea
 const textArea = document.querySelector(".textarea");
+
+// Get start button
+const startButton = document.querySelector(".start");
+
+//element to take in transcribed text
+let spokenWords = document.createElement("span");
+textArea.appendChild(spokenWords);
+
+// For switching mic on/off
+let on = false;
 
 window.SpeechRecognition =
 	window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -13,38 +24,51 @@ const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 recognition.lang = "en-US";
 
-recognition.addEventListener("result", (e) => {
-	console.log(e.results[0][0].transcript);
+function doYourThing(e) {
 	const transcript = Array.from(e.results)
 		.map((result) => result[0])
 		.map((result) => result.transcript)
 		.join("");
 
-	if (e.results[0].isFinal) {
-		textArea.textContent += transcript + " ";
+	if (textArea.textContent !== "") {
+		spokenWords.textContent = ` ${transcript} `;
+	} else {
+		spokenWords.textContent = `${transcript} `;
 	}
-});
+
+	if (e.results[0].isFinal) {
+		spokenWords = document.createElement("span");
+		textArea.appendChild(spokenWords);
+	}
+}
 
 function keepListening() {
 	recognition.start();
 }
 
-const startButton = document.querySelector(".start");
+function startRecognition() {
+	recognition.addEventListener("result", doYourThing);
+	recognition.start();
+	recognition.addEventListener("end", keepListening);
+	on = true;
+	startButton.classList.remove("start");
+	startButton.classList.add("stop");
+}
 
-let on = true;
+function stopRecognition() {
+	recognition.removeEventListener("result", doYourThing);
+	recognition.removeEventListener("end", keepListening);
+	recognition.stop();
+	on = false;
+	startButton.classList.remove("stop");
+	startButton.classList.add("start");
+}
 
+// toggle on and off switch
 startButton.addEventListener("click", () => {
-	if (on) {
-		recognition.start();
-		recognition.addEventListener("end", keepListening);
-		on = false;
-		startButton.classList.remove("start");
-		startButton.classList.add("stop");
-	} else if (!on) {
-		recognition.removeEventListener("end", keepListening);
-		recognition.stop();
-		on = true;
-		startButton.classList.remove("stop");
-		startButton.classList.add("start");
+	if (!on) {
+		startRecognition();
+	} else {
+		stopRecognition();
 	}
 });
