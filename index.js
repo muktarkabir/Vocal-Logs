1900bb17d58994419d436d64ed09e4ae749465d3
muktarkabirgdsc <mukhtarkabir102@gmail.com>
2024-01-08 01:22:44 +0100

    Add a modal for download
    I have added a modal that appears when the download button is clicked
    and also added its appropriate styles and javascript logic. I have also
    restructured my files for hosting purposes.

--- index.js
+++ index.js
@@ -72,3 +72,30 @@ startButton.addEventListener("click", () => {
 		stopRecognition();
 	}
 });
+
+//Download Modal Stuff
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let downloadButton = document.querySelector(".download-file");
+
+// Get the <span> element that closes the modal
+let span = document.querySelector(".close span");
+
+// When the user clicks on the button, open the modal
+downloadButton.onclick = function () {
+	modal.style.display = "flex";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
